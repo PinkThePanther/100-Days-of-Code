3c2dcb1ab909da87cff732434f1b33c139ea88ad
Socartes Arias <socratsz@Socartess-MacBook-Pro.local>
2024-10-22 13:11:25 -0400

    first commit

---
+++ band_name_generator_Day1.py
@@ -0,0 +1,5 @@
+print("Greetings! how are you doing today?")
+city = input("What city did you grow up in?\n")
+pet = input("What's the name of your pet?\n")
+Combined = city + " " + pet
+print(f'Your Band name is {Combined}')
